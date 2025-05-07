721c3b93c1aad973b320ff01d8a5bb0d0ab85cda
sigfalt <1689082+sigfalt@users.noreply.github.com>
2025-05-06 21:54:38 -0500

    feat: add instructional section, shrink text (1440p debuff)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 
 <script lang="ts">
+    import {Input} from "$lib/components/ui/input";
+
     const host = 'tts.borpa.chat';
     const title = `Hannah TTS`;
 
@@ -51,31 +53,41 @@
 <!--    <meta property="og:image" content="https://{host}/favicon.png" />-->
 </svelte:head>
 
-<div id="main" class="text-center text-white m-16 font-germania space-y-10 text-5xl">
-    <h2>Hannah TTS</h2>
+<div id="main" class="text-center text-white m-8 font-germania space-y-8">
+    <h2 class="text-4xl">Hannah TTS</h2>
 
-    <div class="bg-purple-950 p-4 rounded-xl border-2 border-yellow-400">
-        <h3>GIGA Money</h3>
+    <div class="flex flex-row">
+        <div class="basis-1/4"></div>
+        <div class="bg-purple-950 basis-1/2 justify-center items-center p-2 rounded-xl border-2 border-yellow-400">
+            Send a message with the appropriate amounts of bits, your selected voice, and then the message you want the voice to read out.
+            <br/>
+            <Input disabled type="text" placeholder="Cheer100 [sol] I don't do moms. Moms do me. Because I'm a subby little bottom. Shy"></Input>
+        </div>
+        <div class="basis-1/4"></div>
+    </div>
+
+    <div class="bg-purple-950 p-2 rounded-xl border-2 border-yellow-400">
+        <h3 class="text-3xl">GIGA Money</h3>
         <br/>
 
-        <div class="grid grid-cols-3 grid-flow-row-dense gap-8">
+        <div class="grid grid-cols-3 grid-flow-row-dense gap-6">
             {#each deluxe_voices as voice}
                 <div class="bg-slate-900 p-4 rounded-xl border-2 border-yellow-400">
-                    {voice.name}
-                    <p class="text-xl">{voice.price} bits</p>
+                    <p class="text-2xl">{voice.name}</p>
+                    <p class="text-lg">{voice.price} bits</p>
                 </div>
             {/each}
         </div>
     </div>
 
-    <div class="bg-purple-950 p-4 rounded-xl border-2 border-yellow-400">
-        <h3>Standard Voices</h3>
-        <p class="text-xl">100 bits</p>
+    <div class="bg-purple-950 p-2 rounded-xl border-2 border-yellow-400">
+        <h3 class="text-2xl">Standard Voices</h3>
+        <p class="text-lg">100 bits</p>
         <br/>
 
-        <div class="grid grid-cols-4 grid-flow-row-dense gap-8">
+        <div class="grid grid-cols-4 grid-flow-row-dense gap-6">
             {#each standard_voices as voice}
-                <div class="bg-slate-900 p-4 rounded-xl border-2 border-yellow-400">{voice}</div>
+                <div class="bg-slate-900 text-xl p-2 rounded-xl border-2 border-yellow-400">{voice}</div>
             {/each}
         </div>
     </div>
