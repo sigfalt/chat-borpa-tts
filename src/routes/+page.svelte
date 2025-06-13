b92b4543e7a7ba0708c5437e542b6728e63dc996
sigfalt <1689082+sigfalt@users.noreply.github.com>
2025-06-13 17:13:38 -0500

    style: add breakpoint styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -60,42 +60,42 @@
     <meta property="og:image" content="https://{host}/favicon.png" />
 </svelte:head>
 
-<div id="main" class="text-center text-white m-8 font-rowdies space-y-8">
-    <h2 class="text-4xl font-germania">Hannah TTS</h2>
+<div id="main" class="text-center text-white m-8 font-rowdies space-y-4">
+    <h2 class="text-3xl md:text-4xl font-germania">Hannah TTS</h2>
 
     <div class="flex flex-row">
-        <div class="basis-1/4"></div>
-        <div class="bg-purple-950 basis-1/2 justify-center items-center p-2 rounded-xl border-2 border-yellow-400 text-sm">
+        <div class="basis-1/6 md:basis-1/4"></div>
+        <div class="bg-purple-950 basis-2/3 md:basis-1/2 justify-center items-center p-2 rounded-xl border-2 border-yellow-400 text-sm">
             Send a message with the appropriate amounts of bits, your selected voice, and then the message you want the voice to read out.
             <br/>
-            <Textarea disabled
+            <Textarea disabled class="text-xs md:text-sm"
                       placeholder="Cheer100 [sol] I don't do moms. Moms do me. Because I'm a subby little bottom. Shy"/>
         </div>
-        <div class="basis-1/4"></div>
+        <div class="basis-1/6 md:basis-1/4"></div>
     </div>
 
     <div class="bg-purple-950 p-2 rounded-xl border-2 border-yellow-400">
-        <h3 class="text-3xl font-germania">GIGA Money</h3>
-        <br/>
+        <h3 class="text-2xl md:text-3xl font-germania pb-2 md:pt-2 md:pb-4">GIGA Money</h3>
 
-        <div class="grid grid-cols-3 grid-flow-row-dense gap-6">
+        <div class="grid grid-cols-1 md:grid-cols-3 grid-flow-row-dense gap-3 md:gap-6">
             {#each deluxe_voices as voice}
-                <div class="bg-slate-900 p-4 rounded-xl border-2 border-yellow-400">
-                    <p class="text-2xl">{voice.name}</p>
-                    <p class="text-lg">{voice.price} bits</p>
+                <div class="bg-slate-900 p-2 rounded-xl border-2 border-yellow-400">
+                    <p class="text-xl md:text-2xl">{voice.name}</p>
+                    <p class="text-sm md:text-base">{voice.price} bits</p>
                 </div>
             {/each}
         </div>
     </div>
 
     <div class="bg-purple-950 p-2 rounded-xl border-2 border-yellow-400">
-        <h3 class="text-2xl font-germania">Standard Voices</h3>
-        <p class="text-lg">100 bits</p>
-        <br/>
+        <div class="pb-2 md:pt-2 md:pb-4">
+            <h3 class="text-xl md:text-2xl font-germania">Standard Voices</h3>
+            <p class="text-sm md:text-base">100 bits</p>
+        </div>
 
-        <div class="grid grid-cols-4 grid-flow-row-dense gap-6">
+        <div class="grid grid-cols-2 md:grid-cols-4 grid-flow-row-dense gap-3 md:gap-6">
             {#each standard_voices as voice}
-                <div class="bg-slate-900 text-xl p-2 rounded-xl border-2 border-yellow-400">{voice}</div>
+                <div class="bg-slate-900 text-base md:text-xl p-2 rounded-xl border-2 border-yellow-400">{voice}</div>
             {/each}
         </div>
     </div>
@@ -107,14 +107,10 @@
     <div class="basis-1/12"></div>
 </div>
 
-<div class="flex w-screen">
-    <div class="basis-1/3"></div>
-    <div class="basis-1/3 p-4 text-center text-white font-germania text-xs">
-        Not officially associated with <a class="text-yellow-400" href="https://borpa.chat">HannahHyrule</a>
-        <br/>
-        Developed and hosted by <a class="text-yellow-400" href="https://sigfalt.dev">sigfalt</a>
-        <br/>
-        Source available on <a class="text-yellow-400" href="https://github.com/sigfalt/chat-borpa-tts">Github</a>
-    </div>
-    <div class="basis-1/3"></div>
+<div class="p-4 text-center text-white font-germania text-xs">
+    Not officially associated with <a class="text-yellow-400" href="https://borpa.chat">HannahHyrule</a>
+    <br/>
+    Developed and hosted by <a class="text-yellow-400" href="https://sigfalt.dev">sigfalt</a>
+    <br/>
+    Source available on <a class="text-yellow-400" href="https://github.com/sigfalt/chat-borpa-tts">Github</a>
 </div>
