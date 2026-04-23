153789e4bb26aa4ad654fadc8904a9e9399bfab6
sigfalt <1689082+sigfalt@users.noreply.github.com>
2026-04-23 00:33:56 -0500

    feat: fetch existing sound clips from db

--- src/lib/db/d1.service.ts
+++ src/lib/db/d1.service.ts
@@ -7,6 +7,15 @@ export interface Voice {
     enabled: boolean;
 }
 
+export interface AudioFile {
+    audio_id: number;
+    voice_id: number;
+    msg_text: string;
+    s3_path: string;
+    fetch_count: number;
+    req_timestamp: string;
+}
+
 export class VoiceDatabase {
     constructor(private readonly db: D1Database) {}
     
@@ -14,6 +23,13 @@ export class VoiceDatabase {
         return this.db
             .prepare("SELECT * FROM voices WHERE voice_id = ? AND enabled IS TRUE")
             .bind(voice_id)
+            .first();
+    }
+    
+    async searchAudioFile(voice_id: number, msg_text: string): Promise<AudioFile | null> {
+        return this.db
+            .prepare("SELECT * FROM audio_files WHERE voice_id = ? AND msg_text = ?")
+            .bind(voice_id, msg_text)
             .first();
     }
 }
--- src/lib/tts.remote.ts
+++ src/lib/tts.remote.ts
@@ -1,5 +1,6 @@
 import * as v from 'valibot';
 import {command, getRequestEvent} from "$app/server";
+import {error} from "@sveltejs/kit";
 
 const TTSSchema = v.object({
     voice_id: v.pipe(
@@ -12,14 +13,26 @@ const TTSSchema = v.object({
 export const getVoice = command(TTSSchema, async (cmd_obj) => {
     const { locals } = getRequestEvent();
     console.log(`getVoice command: ${JSON.stringify(cmd_obj)}`);
-    let { voice_id, tts_msg } = cmd_obj;
+    const { voice_id, tts_msg } = cmd_obj;
     
     // check db with voice_id
-    let voice_record = await locals.db_service.getVoice(voice_id);
+    const voice_record = await locals.db_service.getVoice(voice_id);
     console.log(`Voice record: ${JSON.stringify(voice_record)}`);
+    if (!voice_record) {
+        error(400, 'Invalid voice provided.');
+    }
     
     // check if message and voice combo already generated and fetch from S3
-    // otherwise generate sound clip with elevenlabs
-    // save file to S3 bucket and store in db
+    const audio_file = await locals.db_service.searchAudioFile(voice_record!.voice_id, tts_msg);
+    console.log(`Audio record: ${JSON.stringify(audio_file)}`);
+    
+    let audio_data;
+    if (audio_file) {
+        audio_data = audio_file.s3_path;
+    } else {
+        // otherwise generate sound clip with elevenlabs
+        // save file to S3 bucket and store in db
+    }
+    
     // return voice clip
 });
