e911ed4cc9fef3fe59e7df1bb00288f349a4aafc
sigfalt <1689082+sigfalt@users.noreply.github.com>
2026-05-09 23:43:24 -0500

    test: test service imports construction

--- src/lib/hooks/hooks.server.ts
+++ src/lib/hooks/hooks.server.ts
@@ -4,8 +4,8 @@ import {AudioStorage} from "$lib/s3/r2.service";
 import {ElevenLabsClient} from "@elevenlabs/elevenlabs-js";
 
 export const handle: Handle = async ({ event, resolve }) => {
-    event.locals.db_service = new VoiceDatabase(event.platform!.env.VOICES_DB);
-    event.locals.s3_service = new AudioStorage(event.platform!.env.VOICES_S3);
-    event.locals.tts_service = new ElevenLabsClient({ apiKey: event.platform!.env.ELEVENLABS_API_KEY });
+    // event.locals.db_service = new VoiceDatabase(event.platform!.env.VOICES_DB);
+    // event.locals.s3_service = new AudioStorage(event.platform!.env.VOICES_S3);
+    // event.locals.tts_service = new ElevenLabsClient({ apiKey: event.platform!.env.ELEVENLABS_API_KEY });
     return resolve(event);
 }
