0d7987789badfaad4e548aabae7927983afadd65
sigfalt <1689082+sigfalt@users.noreply.github.com>
2026-05-16 20:56:20 -0500

    fix: store raw audio stream response and convert to blob as needed

--- src/lib/tts.remote.ts
+++ src/lib/tts.remote.ts
@@ -26,17 +26,19 @@ export const getVoice = command(TTSSchema, async (cmd_obj) => {
     const audio_record = await locals.db_service.searchAudioFile(voice_record!.voice_id, tts_msg);
     console.log(`Audio record: ${JSON.stringify(audio_record)}`);
     
-    let audio_blob;
+    let response_blob;
     if (audio_record) {
         // fetch pregenerated audio from s3
         const s3_path = audio_record.s3_path;
-        const fetched_blob = await locals.s3_service.get(s3_path);
+        const fetched_stream = await locals.s3_service.get(s3_path);
         
-        if (!fetched_blob) {
+        if (!fetched_stream) {
             error(500, 'Pregenerated audio file not found.');
         }
         await locals.db_service.incrementAudioFetchCount(audio_record.audio_id);
-        audio_blob = fetched_blob;
+        response_blob = await new Response(fetched_stream, {
+            headers: { 'Content-Type': 'audio/mpeg' },
+        }).blob();
     } else {
         // otherwise generate new audio with elevenlabs
         const audio_stream = await locals.tts_service.textToSpeech.convert(
@@ -46,21 +48,21 @@ export const getVoice = command(TTSSchema, async (cmd_obj) => {
                 // default outputFormat is mp3_44100_128, other values may be plan tier locked
                 // outputFormat: 'mp3_44100_128'
             });
-        audio_blob = await new Response(audio_stream, {
-            headers: { 'Content-Type': 'audio/mpeg' },
-        }).blob();
         
         // save audio blob to S3 bucket and store in db
         const s3_path = crypto.randomUUID();
-        const blob_stored = await locals.s3_service.put(s3_path, audio_blob);
-        if (blob_stored) {
+        const audio_stored = await locals.s3_service.put(s3_path, audio_stream);
+        if (audio_stored) {
             await locals.db_service.insertNewAudio(voice_record.voice_id, tts_msg, s3_path);
         } else {
             console.warn(`Failed to store audio blob for UUID {s3_path}`);
         }
         // even if blob storage failed, return generated audio to user
+        response_blob = await new Response(audio_stream, {
+            headers: { 'Content-Type': 'audio/mpeg' },
+        }).blob();
     }
     
     // return voice clip
-    return audio_blob;
+    return response_blob;
 });
